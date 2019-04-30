c6625440c9b261fe7ac28b521cd170395ec231f9
Willem Jiang <jiangning9@huawei.com>
2019-04-30 17:19:24 +0800

    Added camel-rabbitmq-demo

---
+++ camel-rabbitmq-demo/src/main/java/com/example/camel/demo/CamelRoute.java
@@ -0,0 +1,35 @@
+package com.example.camel.demo;
+
+import org.apache.camel.builder.RouteBuilder;
+
+import org.apache.camel.component.rabbitmq.RabbitMQComponent;
+import org.springframework.stereotype.Component;
+
+import com.rabbitmq.client.ConnectionFactory;
+
+@Component
+public class CamelRoute extends RouteBuilder {
+
+  @Override
+  public void configure() throws Exception {
+    //We could setup the rabbitmq connection here
+    ConnectionFactory connectionFactory = new ConnectionFactory();
+    connectionFactory.setHost("localhost");
+    connectionFactory.setPort(5672);
+    connectionFactory.setUsername("guest");
+    connectionFactory.setPassword("guest");
+    RabbitMQComponent component = new RabbitMQComponent();
+    component.setConnectionFactory(connectionFactory);
+    
+      
+    getContext().addComponent("rabbitmq-1", component);
+
+
+    from("timer:hello?period=1000")
+        .transform(simple("Random number ${random(0,100)}"))
+        .to("rabbitmq-1:foo");
+
+    from("rabbitmq-1:foo")
+          .log("From RabbitMQ: ${body}");
+  }
+}
